0427cc01f03cbe41f60c6c91edace8bbf38372f7
duong.duc <duc.dd108@gmail.com>
2025-01-12 22:48:31 +0700

    build weight tracker

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Nav from './components/Nav.vue';
+import Home from './components/Home.vue';
+
+</script>
+
+<template>
+  <Nav></Nav>
+  <Home></Home>
+</template>
+
+<style>
+
+</style>
